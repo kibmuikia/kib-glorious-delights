935cafb15802953d7351fbe77e99bdcd6b4dcd9b
kibmuikia <muikiaa@gmail.com>
2019-06-30 01:47:07 +0300

    after major base config changes, uploadig to heroku

---
+++ dist/precache-manifest.fac4ce151f96a124646a38a9cbffe889.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ab8bec70ae9ddfd50083",
+    "url": "/js/chunk-vendors.0da48cf0.js"
+  },
+  {
+    "revision": "d4538e5f8e41089897ff",
+    "url": "/js/chunk-49420c58.82a684f4.js"
+  },
+  {
+    "revision": "78f98d0f56681c7e43ba",
+    "url": "/js/chunk-2a66e1b6.afa692d5.js"
+  },
+  {
+    "revision": "e2734d4d7dc7590be308",
+    "url": "/js/app.6701923d.js"
+  },
+  {
+    "revision": "ace1a05b667a8cdd25d0d889fb91443a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab8bec70ae9ddfd50083",
+    "url": "/css/chunk-vendors.eb519f21.css"
+  },
+  {
+    "revision": "d4538e5f8e41089897ff",
+    "url": "/css/chunk-49420c58.7f589c6e.css"
+  },
+  {
+    "revision": "78f98d0f56681c7e43ba",
+    "url": "/css/chunk-2a66e1b6.036d215c.css"
+  },
+  {
+    "revision": "e2734d4d7dc7590be308",
+    "url": "/css/app.0f2cad9c.css"
+  }
+];
